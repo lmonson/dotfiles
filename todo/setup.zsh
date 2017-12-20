97ec3dab7c87df6bea734e3c2d2d3e85c6edc901
lmonson <lmpub01@gmail.com>
2017-12-19 21:56:05 -0700

    minor tweaks

--- todo/setup.zsh
+++ todo/setup.zsh
@@ -1,2 +1,4 @@
 source /usr/local/Cellar/todo-txt/2.10/etc/bash_completion.d/todo_completion complete -F _todo t
 alias t='/usr/local/bin/todo.sh -d $HOME/.todo.conf'
+alias tcode='code $HOME/Documents/notes/todo/todo.txt'
+
