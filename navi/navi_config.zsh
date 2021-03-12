8f5c7679ba855e952f5a75a40d1c1740011e821b
lmonson <lmonson@computer.org>
2021-03-11 19:52:10 -0700

    Added more sophisticated navi setup

--- navi/navi_config.zsh
+++ navi/navi_config.zsh
@@ -1,9 +1,20 @@
-# zsh
-source <(echo "$(navi widget zsh)")
+_call_navi() {
+  local selected
+  CUSTOM_PATH="./.cheats:$(navi info cheats-path)"
+  echo $CUSTOM_PATH
+  if [ -n "$LBUFFER" ]; then
 
-# '/System/Volumes/Data/Users/lmonson/Library/Application Support/navi/cheats'
-
-function po() {
-  print -z `navi --print`
+    if selected="$(printf "%s" "$(navi --path "$CUSTOM_PATH" --print --fzf-overrides '--no-select-1' --query "${LBUFFER}" </dev/tty)")"; then
+      LBUFFER="$selected"
+    fi
+  else
+    if selected="$(printf "%s" "$(navi --path "$CUSTOM_PATH" --print </dev/tty)")"; then
+      LBUFFER="$selected"
+    fi
+  fi
+  zle redisplay
 }
 
+zle -N _call_navi
+
+bindkey '^g' _call_navi
