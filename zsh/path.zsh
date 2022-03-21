80fabd351e864d717797d5a7956979437696590f
lmonson <>
2022-03-21 16:23:57 -0600

    Added a few more utilities

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -1,2 +1,3 @@
 #export PATH=$/usr/local/bin:$PATH:/usr/local/homebrew/bin
-export PATH="/usr/local/sbin:/usr/local/bin:/usr/bin:$PATH"
+#export PATH="/usr/local/sbin:/usr/local/bin:/usr/bin:$PATH"
+export PATH="/opt/homebrew/sbin:/opt/homebrew/bin:/usr/bin:$PATH"
