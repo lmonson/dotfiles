109896aa953513b350da807f6ab3fb6b77b713a4
lmonson <>
2022-05-28 06:08:43 -0600

    Updates

---
+++ terraform/terraform.zsh
@@ -0,0 +1,8 @@
+alias tr='rm -rf .terra* the_plan'
+alias ti='terraform init'
+alias tp='terraform plan -out=the_plan'
+alias ta='terraform apply the_plan'
+alias tpo="gsed -i 's/role_arn/#role_arn/' provider.tf"
+alias tpi="gsed -i 's/role_arn/#role_arn/' provider.tf"
+
+
